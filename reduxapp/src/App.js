a678c430865a9d77b2f7065d21930c8e89b3ba03
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-20 23:39:34 +0530

    Update App.js

--- reduxapp/src/App.js
+++ reduxapp/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
+import { useSelector, useDispatch} from "react-redux";
+import { incNumber, decNumber, chgName} from "./action/index"
+
 function App() {
+  const myState = useSelector((state)=> state.changeNumber)
+  const myNameState = useSelector((state)=> state.changeName)
+  const dispatch = useDispatch()
   return (
     <>
       <h2>Increment and Decrement</h2>
-      <button>+</button><span>0</span><button>-</button>
+      <button onClick={()=> dispatch(incNumber())}>+</button>
+      <span>{myState}</span>
+      <button onClick={()=> dispatch(decNumber())}>-</button>
+      <span>{myNameState}</span>
+      <button onClick={()=> dispatch(chgName())}>Change Name</button>
     </>
   );
 }
