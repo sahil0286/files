042050aee69fd81f70f87d1cf9797271240900cd
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-10-12 22:40:34 +0530

    Update

--- js/test.js
+++ js/test.js
@@ -1,3 +1,10 @@
-let a=10;
-let b=20;
-console.log(a+b)
+// let a="20";
+// let b="10";
+// console.log(a*b)
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a+b)
+
+
+let x = Number.MAX_SAFE_INTEGER;
+console.log(x)
